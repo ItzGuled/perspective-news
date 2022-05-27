499970c25a521699f45750b9dfb109e10779e790
Glorzo <thisaraeranga@gmail.com>
2022-05-26 20:51:24 -0500

    resolvers completed

--- Server/schemas/resolvers.js
+++ Server/schemas/resolvers.js
@@ -15,8 +15,51 @@ const resolvers = {
         }
     },
     Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
+            if(!user) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return{ token, user }
+        },
+        saveNews: async (parent, args, context) => {
+            if(context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    {$addToSet: { savedNews: args }},
+                    {new: true, runValidators: true}
+                )
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in to save news!');
+        },
+        removeNews: async (parent, { newsId }, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                {_id: context.user._id},
+                {$pull: {savedNews: {_id: newsId}}},
+                {new: true}
+            );
+
+            if(!updatedUser) {
+                throw new AuthenticationError("Couldn't find the user")
+            }
+        }
     }
-}
+};
 
 module.exports = resolvers;
