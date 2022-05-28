ad31ef16ee32f35e0d253557c86ec791dc4d6a56
rioobod <akanbilola@gmail.com>
2022-05-27 19:57:13 -0500

    replaced header and footer with header and footer folders with js and css files within, edited App to reflect changes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 import Nav from "./components/Navbar";
 import Search from "./pages/Search";
 
