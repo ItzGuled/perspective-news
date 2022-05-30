527776ce2c88add9d4d7a511c4433adccd4837ec
Pat Lawler <patlaw777@icloud.com>
2022-05-30 11:22:58 -0500

    updating login form

--- Client/src/components/LoginForm.js
+++ Client/src/components/LoginForm.js
@@ -1,104 +1,79 @@
-import React, { useState } from "react";
-import { Form, Button, Alert } from "react-bootstrap";
-import { useMutation } from "@apollo/react-hooks";
+import React, {useState } from "react";
+import Auth from '../utils/auth';
 
-import { LOGIN_USER } from "../utils/mutations";
-import Auth from "../utils/auth";
 
 const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [login, { error }] = useMutation(LOGIN_USER);
 
+  // use mutation for login route
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
+  
+  
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+
   const handleFormSubmit = async (event) => {
+
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
     try {
-      const response = await login(userFormData);
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
+
+      if (error) {
+        throw new Error('something went wrong!');
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
+
     } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      console.error(`
+      ===========================
+      ERROR
+      ===========================
+      ${err}
+      `);
+
+      // setShowAlert(true);
     }
 
     setUserFormData({
-      username: "",
-      email: "",
-      password: "",
+      username: '',
+      email: '',
     });
   };
 
-  return (
-    <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {error ? (
-          <Alert
-            dismissible
-            onClose={() => setShowAlert(false)}
-            show={showAlert}
-            variant="danger"
-          >
-            Something went wrong with your login credentials!
-          </Alert>
-        ) : null}
-        <Form.Group>
-          <Form.Label htmlFor="email">Email</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your email"
-            name="email"
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Email is required!
-          </Form.Control.Feedback>
-        </Form.Group>
 
-        <Form.Group>
-          <Form.Label htmlFor="password">Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Your password"
-            name="password"
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Password is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type="submit"
-          variant="success"
-        >
-          Submit
-        </Button>
-      </Form>
-    </>
+
+  console.log(userFormData)
+
+
+  return (
+    <div>
+      <input
+        name="email" 
+        placeholder="email"
+        onChange={handleInputChange}
+        />
+      <input
+        name="password"
+        placeholder="password"
+        onChange={handleInputChange}
+        />
+      <button
+        disabled={!(userFormData.email && userFormData.password)}
+        type='submit'
+        onClick={handleFormSubmit}>
+        Login
+      </button>
+    </div>
   );
 };
 
