38ea2926bf7976e0de47b3265612ffd576d40a41
Glorzo <thisaraeranga@gmail.com>
2022-05-31 20:14:19 -0500

    heroku deploy

--- Server/server.js
+++ Server/server.js
@@ -26,11 +26,11 @@ const server = new ApolloServer({
 
     // Serve up static assets
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/build')));
+        app.use(express.static(path.join(__dirname, '../Client/build')));
     }
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+        res.sendFile(path.join(__dirname, '../Client/build/index.html'));
     });
 
 
